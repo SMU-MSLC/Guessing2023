ed6229ea7aa2a4fbe6f76e3d6f3cf1628a3f7f51
Eric Larson <eclarson@smu.edu>
2023-09-06 14:38:12 -0500

    Updated to work in class

--- Guessing2023/GuessingViewController.swift
+++ Guessing2023/GuessingViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class GuessingViewController: UIViewController, UITextFieldDelegate {
-
+    @IBOutlet weak var feedbackLabel: UILabel!
+    
     var guessModel = GuessModel()
     
     @IBOutlet weak var guessNumberTextField: UITextField!
@@ -17,16 +18,50 @@ class GuessingViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(guessModel.correctNum)
         
         guessNumberTextField.delegate = self
+        guessNumberTextField.becomeFirstResponder()
     }
     
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        self.guessNumberTextField.resignFirstResponder()
+        
         return true
+    }
+    
+    @IBAction func makeGuess(){
+        var textToDisplay = ""
+        
+        if let userGuess = self.guessNumberTextField.text,
+            let userGuessInt = Int(userGuess),
+            userGuessInt <= guessModel.upperBound(),
+            userGuessInt >= guessModel.lowerBound(){
+            
+            
+            
+            switch guessModel.makeGuess(userGuessInt){
+            case GuessValueCorrect:
+                textToDisplay = "Correct!"
+                self.guessNumberTextField.resignFirstResponder()
+            case GuessValueLower:
+                textToDisplay = "Lower!"
+            case GuessValueHigher:
+                textToDisplay = "Higher!"
+            default:
+                textToDisplay = "never will get here"
+            }
+            
+        }else{
+            textToDisplay = "please enter a number between \(guessModel.lowerBound()) and \(guessModel.upperBound())"
+        }
+        
+        self.guessNumberTextField.text = ""
+        self.feedbackLabel.text = textToDisplay
+    }
+    
+    @IBAction func tapDidCancel(_ sender: UITapGestureRecognizer) {
+        self.guessNumberTextField.resignFirstResponder()
     }
     
 
